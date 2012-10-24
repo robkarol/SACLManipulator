4a51890940b7445595dcb601f5ec6c5b7834c755
Robert Karol <Robert.Karol@ijkco.com>
2012-10-24 16:20:53 -0700

    added linked_list files

--- linked_list.c
+++ linked_list.c
@@ -1,6 +1,144 @@
-/*! Structure for linked lists. Structure variable used to represent the element of a linked list of integer data.  List nodes are employed primarily to
-store lists of subtree leaves for each node in an RRT data structure. */
-struct list_node {
-	int data;						/*!< Integer data associated with the list node */
-	struct list_node *next;			/*!< Pointer to the next linked list element (or NULL at end of list) */
-};
+/*
+ * FILE: linked_list.c
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "memcheck.h"
+#include "linked_list.h"
+
+
+/*
+ * create_node:
+ *     Create a single node and link it to the node called 'n'.
+ */
+
+node *create_node(int data, node *n) {
+  node *result = (node *)malloc(sizeof(node));
+
+  if(result == NULL) {
+    fprintf(stderr, "Fatal error: out of memory. "
+                    "Terminating program.\n");
+    exit(1);
+  }
+
+  result->data = data;  /* Fill in the new node with the given value. */
+  result->next = n;
+
+  return result;
+}
+
+
+/*
+ * free_list:
+ *     Free all the nodes of a linked list.
+ */
+
+void free_list(node *list) {
+  node *n;     /* a single node of the list */
+
+  while (list != NULL) {
+    n = list;
+    list = list->next;
+    free(n);
+  }
+}
+
+
+/*
+ * copy_list:
+ *     Return a copy of a list.
+ */
+
+node *copy_list(node *list) {
+  node *new_list;
+  if (list == NULL) {
+    return list;
+  }
+  else {
+    new_list = create_node(list->data,copy_list(list->next));
+    return new_list;
+  }
+}
+
+
+/*
+ * append_lists:
+ *     Return a list which is a copy of the concatenation of the two
+ *     input lists.
+ */
+
+node *append_lists(node *list1, node *list2) {
+  node *item;
+
+  if(list1 == NULL) {
+    return copy_list(list2);
+  }
+  else if (list2 == NULL) {
+    return copy_list(list1);
+  }
+  else {
+    node *new_list1 = copy_list(list1);
+    node *new_list2 = copy_list(list2);
+
+    for (item = new_list1; item != NULL; item = item->next) {
+      if (item->next == NULL)  /* We're on the last node. */
+      {
+        item->next = new_list2;
+        break;
+      }
+    }
+    return new_list1;
+  }
+}
+
+
+/*
+ * Make a reversed copy of a list.
+ */
+
+node *reverse_list(node *list) {
+  node *item;
+  node *reversed_list = NULL;
+
+  for(item = list; item != NULL; item = item->next) {
+    reversed_list = create_node(item->data, reversed_list);
+  }
+
+  return reversed_list;
+}
+
+
+/*
+ * Print the elements of a list.
+ */
+
+void print_list(node *list) {
+  node *item;
+
+  for (item = list; item != NULL; item = item->next) {
+    printf("%d\n", item->data);
+  }
+}
+
+/*
+ * Return 1 if a list is sorted, otherwise 0.
+ * Useful for debugging.
+ */
+
+int is_sorted(node *list) {
+  node *item;
+
+  /* An empty list is sorted by definition. */
+  if (list == NULL)
+    return 1;
+
+  for (item = list; item->next != NULL; item = item->next) {
+    if (item->data > item->next->data)  /* wrong order */
+      return 0;
+  }
+
+  return 1;  /* The list is sorted. */
+}
+
